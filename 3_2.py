ba097689962138b7cd7306679ce7a9f8ceecc796
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-05-06 22:04:53 +0300

    Release version1.

--- 3_2.py
+++ 3_2.py
@@ -1,16 +1,20 @@
 # Пользователь вводит 3 числа, надо найти среднее арифмитическое с
 # точностью 3 знака после запятой.
 
-first_num = input('Enter any integer: ')
-second_num = input('Enter any integer: ')
-third_num = input('Enter any integer: ')
+first_num = float(input('Enter any integer: '))
+second_num = float(input('Enter any integer: '))
+third_num = float(input('Enter any integer: '))
 
 # 1
-def mean_3nums(first_num, second_num, third_num):
-    """"Returns the mean of 3 arguments"""
-    sum_num = float(first_num) + float(second_num) + float(third_num)
-    return round(sum_num/3, 3)
-print(mean_3nums(first_num, second_num, third_num))
+def mean_nums(*args):
+    """"Returns the mean of all arguments"""
+    sum_num = 0
+    for num in args:
+        sum_num += num
+    return sum_num/len(args)
+
+print('The mean of the numbers you entered is ' + str(round(mean_nums(first_num, second_num, third_num), 3)) + '.')
+
 
 # 2
 print(round((float(first_num) + float(second_num) + float(third_num))/3, 3))
