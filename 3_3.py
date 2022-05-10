2b5477fe407791166eaae307a2f39a1a22d94de4
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-05-10 11:43:35 +0300

    Release version4.

--- 3_3.py
+++ 3_3.py
@@ -3,13 +3,13 @@
 
 # 1 f-str
 first_name = input('Enter your first name: ')
-age = input('Enter your first age: ')
+age = input('Enter your age: ')
 city = input('Enter the city where you live: ')
 
 print(f"Hi! I am {first_name} from {city}. I am {age} years old.")
 
 # 2 concatenation
-print('Hi! ' + 'I am ' + str(first_name) + ' from ' + str(city) + '. I am ' + str(age) + ' years old.')
+print('Hi! ' + 'I am ' + first_name + ' from ' + city + '. I am ' + age + ' years old.')
 
 # 3 str.format()
 print('Hi! I am {} from {}. I am {} years old.'.format(first_name, city, age))
