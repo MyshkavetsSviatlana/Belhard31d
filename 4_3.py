41b207ec1844e4d0a6281fb9983591f236b7c65f
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-05-12 13:27:03 +0300

    Release version2.

--- 4_3.py
+++ 4_3.py
@@ -4,10 +4,22 @@
 
 n = int(input('Enter any integer: '))
 
-outer_dict = {}
-for i in range(0, n + 1):
-    inner_dict = {}
-    outer_dict[i] = inner_dict
-    inner_dict['name'] = input('Enter your name: ')
-    inner_dict['email'] = input('Enter your email: ')
-    print(outer_dict)
+# # 1
+# outer_dict = {}
+# for i in range(0, n + 1):
+#     inner_dict = {}
+#     outer_dict[i] = inner_dict
+#     inner_dict['name'] = input('Enter your name: ')
+#     inner_dict['email'] = input('Enter your email: ')
+#     print(outer_dict)
+#
+# # 2
+# outer_dict1 = {}
+# for i in range(0, n):
+#     text = {'name': input('Enter your name: '), 'email': input('Enter your email: ')}
+#     outer_dict1[i] = text
+# print(outer_dict1)
+
+# 3
+users_dict = {i: {'name': input('Enter your name: '), 'email': input('Enter your email: ')} for i in range(0, n)}
+print(users_dict)
