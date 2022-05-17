007ce0da3fdaf3f78eb0046058fcce044dd9bf6f
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-05-17 21:28:24 +0300

    Release version1.

--- 6_1.py
+++ 6_1.py
@@ -15,24 +15,28 @@
 # print(decimal_to_binary(126))
 
 def decimal_to_binary(a):
-    sequence = []
-    b = 1
-    while len(sequence) < 16:
-        sequence.append(a/b)
-        b *= 2
-    sequence_str = [str(i) for i in sequence]
-    sequence_str_whole = [i[:i.find('.')] for i in sequence_str]
+    if a == 0:
+        return 0
+    else:
 
-    result = ''
-    for i in sequence_str_whole:
-        if float(i) % 2 == 0:
-            result += '0'
-        else:
-            result += '1'
-    return print(result[::-1])
+        sequence = []
+        b = 1
+        while a/b >= 1:
+            sequence.append(a/b)
+            b *= 2
+        sequence_str = [str(i) for i in sequence]
+        sequence_str_whole = [i[:i.find('.')] for i in sequence_str]
+
+        result = ''
+        for i in sequence_str_whole:
+            if float(i) % 2 == 0:
+                result += '0'
+            else:
+                result += '1'
+        return print(result[::-1])
 
 
-print(decimal_to_binary(126))
+print(decimal_to_binary(2))
 
 
 
