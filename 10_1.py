0f4bc9eff1981e68b5e8bd9e4dd0b09db91fa6b3
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-06-02 21:04:18 +0300

    Release version.

---
+++ 10_1.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+from requests import Session
+from http import HTTPStatus
+
+
+def annotation(markdown):
+    annotation = {"body": {"markdown": markdown}}
+    return annotation
+
+
+def referent(raw_annotatable_url, fragment, before_html, after_html):
+    referent = {'raw_annotatable_url': raw_annotatable_url, 'fragment': fragment,
+                'context_for_display': {'before_html': before_html, 'after_html': after_html}}
+    return referent
+
+
+def web_page(canonical_url, og_url, title):
+    web_page = {"canonical_url": canonical_url, "og_url": og_url, "title": title}
+    return web_page
+
+
+class GeniusRequest(BaseModel):
+    annotation = annotation(str)
+    referent = referent(any, any, any, any)
+    web_page = web_page(any, any, str)
+
+
+def get_response(data: GeniusRequest):
+    with Session() as session:
+        response = session.get(
+            url='api.genius.com/web_pages/lookup?',
+            params=data.dict()
+        )
+        if response == HTTPStatus.OK:
+            return response.json()
+
+
+data = GeniusRequest(annotation=annotation("hello **world!**"),
+                     referent=referent("http://seejohncode.com/2014/01/27/vim-commands-piping/",
+                                       "execute commands", "You may know that you can ",
+                                       " from inside of vim, with a vim command:"),
+                     web_page=web_page(None, None, 'Secret of Mana'))
+print(data)
+# print(get_response(data))
